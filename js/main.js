e991798156c38f7a0d03a9983d924e522ef86eff
Andrea <andrea_dao@berkeley.edu>
2026-02-14 01:39:40 -0800

    ooh image fade in

--- js/main.js
+++ js/main.js
@@ -6,3 +6,27 @@ document.addEventListener('DOMContentLoaded', () => {
     mobileMenu.classList.toggle('active')
   );
 });
+
+/*
+// make observer
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            // add class when visible
+            entry.target.classList.add('show'); 
+        } else {
+            // remove when not visible (so it animates again)
+            entry.target.classList.remove('show');
+        }
+    });
+}, {
+  threshold: 0, // as soon as 1 px visible
+});
+
+// select hidden elements
+const hiddenElements = document.querySelectorAll('.hidden');
+
+// observer, watch the hidden elements:
+hiddenElements.forEach((el) => observer.observe(el));
+
+*/
